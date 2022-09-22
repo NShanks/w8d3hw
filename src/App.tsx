13ef0317c337e72d9b36484c15ac66a7f85f680a
Nicolas Shanks <nicolasshanks10@gmail.com>
2022-09-22 10:12:27 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Console } from 'console';
+import type { FormEvent } from 'react'
 import './App.css';
+
+
+const sendForm = async (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault()
+
+  const { name, password } = event.target as typeof event.target & {
+    name: { value: string }
+    password: { value: string}
+  }
+  
+
+  console.log(name.value, password.value)
+// Below is the code to implement it into a database.
+//   await fetch('/route', {
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     method: 'POST',
+//     body: JSON.stringify({
+//       name: name.value,
+//       password: password.value
+//     })
+//   })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="wrap" onSubmit={evt => { sendForm(evt) }}>
+      <fieldset className="field-area">
+        <label htmlFor='name'>Name: </label>
+        <input type="text" id="name" />
+      </fieldset>
+      <fieldset className="field-area">
+        <label htmlFor='password'>Password: </label>
+        <input type="password" id="password" />
+      </fieldset>
+      <button type='submit'>Login</button>
+    </form>
   );
 }
 
